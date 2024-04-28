5f847aefebaa0d61003924b39115e6656d854d65
ivanwijdan16 <ivanok152@gmail.com>
2024-04-28 19:45:04 +0700

    add: modul & praktikum_06

---
+++ praktikum_06/lib/ui/home_screen.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: use_super_parameters, prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:praktikum_06/ui/login.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "HOME SCREEN",
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff3D4DE0)),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                GoogleSignIn().signOut();
+                FirebaseAuth.instance
+                    .signOut()
+                    .then((value) => Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                        (route) => false));
+              },
+              child: const Text('Keluar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ praktikum_06/lib/utils/routes.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers, prefer_const_constructors, unused_local_variable, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:praktikum_06/ui/home_screen.dart';
+import 'package:praktikum_06/ui/login.dart';
+import 'package:praktikum_06/ui/register.dart';
+
+MaterialPageRoute _pageRoute(
+        {required Widget body, required RouteSettings settings}) =>
+    MaterialPageRoute(builder: (_) => body, settings: settings);
+Route? generateRoute(RouteSettings settings) {
+  Route? _route;
+  final _args = settings.arguments;
+  switch (settings.name) {
+    case rLogin:
+      _route = _pageRoute(body: LoginScreen(), settings: settings);
+      break;
+    case rRegister:
+      _route = _pageRoute(body: RegisterScreen(), settings: settings);
+      break;
+    case rHome:
+      _route = _pageRoute(body: HomeScreen(), settings: settings);
+      break;
+  }
+  return _route;
+}
+
+final NAV_KEY = GlobalKey<NavigatorState>();
+const String rLogin = '/login';
+const String rRegister = '/register';
+const String rHome = '/home';
